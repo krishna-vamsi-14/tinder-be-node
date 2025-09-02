3123355156b075d6932e81b8b6cb5c49bc78d312
krishna-vamsi-14 <durgakrishnavamsi@gmail.com>
2025-09-03 01:07:07 +0530

    added patch api

--- src/app.js
+++ src/app.js
@@ -84,6 +84,29 @@ app.delete("/users/:id", async (req, res) => {
     }
 })
 
+app.patch("/users/:id", async (req, res) => {
+    const userId = req.params.id;
+    const updatedData = req.body;
+
+    try {
+        const user = await UserModel.findByIdAndUpdate(userId, updatedData);
+        if(!user) {
+            res.status(404).send({
+                success: false,
+                message: 'User not found',
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: 'User updated successfully',
+            data: user,
+        });
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+})
+
 app.use("/", (err, req, res, next) => {
   res.status(500).send('Something went wrong!! Please try again later.');
 });
