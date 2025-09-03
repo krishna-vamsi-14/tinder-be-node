ea73a620f6997b322bc973f0e0c5568caa8e04f9
krishna-vamsi-14 <durgakrishnavamsi@gmail.com>
2025-09-03 18:03:54 +0530

    added validator package

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const validator = require('validator');
+
 
 const userSchema = new Schema({
     firstName: {
@@ -21,9 +23,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true,
         validate: {
-            validator: function(value) {
-                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-            },
+            validator: validator.isEmail,
             message: 'Invalid email address',
         },
     },
